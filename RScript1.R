335262eea968c0c27e1efc5b3f3be5c68bf734ce
Jason Cho <bc454@cornell.edu>
2017-10-20 15:29:20 -0400

    10/20/2017

--- RScript1.R
+++ RScript1.R
@@ -4,6 +4,7 @@
 library(readr)
 Extracted_features <- read_csv("C:/Users/bj091/Desktop/CS 4786/comp1/Extracted_features.csv", 
                                col_names = FALSE)
+
 Extracted_features <- matrix(as.matrix(Extracted_features),ncol=1084, nrow=10000,dimnames=NULL)
 
 initial_centroids <- read_csv("C:/Users/bj091/Desktop/CS 4786/Kaggle 1/Nuclear-dolphin/centriods.csv",
@@ -13,6 +14,10 @@ initial_centroids <- matrix(as.matrix(initial_centroids),ncol=1084, nrow=10,dimn
 
 rownum<-matrix(c(1:10000),nrow=10000,ncol=1)
 
+Seed <- read_csv("C:/Users/bj091/Desktop/CS 4786/comp1/Seed.csv",col_names = FALSE)
+
+Seed <- matrix(as.matrix(Seed),ncol=2, dimnames = NULL)
+
 #running k-means algorithm (Lloyd algorithm) without the initial clustering from "Graph"
 
 cluster_wo<-kmeans(Extracted_features,algorithm = "Lloyd",centers=10,iter.max = 10000)$cluster
@@ -20,6 +25,14 @@ cluster_wo<-kmeans(Extracted_features,algorithm = "Lloyd",centers=10,iter.max = 
 
 cluster_with<-kmeans(Extracted_features,algorithm = "Lloyd",centers=initial_centroids,iter.max = 10000)$cluster
 
+#matching datapoint and seed
 
+seed_rownum<-merge(rownum,Seed,all.x=TRUE)
+
+datapoints<-cbind(seed_rownum,cluster_with,cluster_wo)
+
+write.table(cluster_wo, file = "cluster_wo.csv",row.names=FALSE, na="",col.names=FALSE, sep=",")
+
+write.table(cluster_with, file = "cluster_with.csv",row.names=FALSE,na="",col.names=FALSE, sep=",")
 
 
