6d344f136aae74e80b678087437a613486fcaa88
Brent Allard <brentallard@dhcp-hol-3574.redrover.cornell.edu>
2017-10-23 19:59:04 -0400

    plots for elbow method

--- pca.py
+++ pca.py
@@ -1,9 +1,15 @@
 import numpy as np
 from sklearn.decomposition import PCA
+import os
+from matplotlib import pyplot
 
 K = 20
 
 # load txt suggested on stackoverflow
+try:
+    os.mkdir('pca_data')
+except FileExistsError:
+    pass
 data_matrix = np.asmatrix(np.loadtxt(open("data/Extracted_features.csv", "rb"), delimiter=","))
 pca = PCA(n_components=K)
 new_matrix = pca.fit_transform(data_matrix)
@@ -12,12 +18,17 @@ np.savetxt('pca_data/pca{}.csv'.format(K), new_matrix, delimiter=',')
 
 # data_mu = np.average(data_matrix, axis=1)
 #
-# cov_matrix = np.asmatrix(np.cov(data_matrix, rowvar=False))
-#
-# eigvals, eigvecs = np.linalg.eig(cov_matrix)
-# eiglist = [(eigval, eigvec,) for eigval, eigvec in zip(eigvals, eigvecs)]
-# eiglist.sort() # low to high
-# eiglist.reverse() # high to low
+cov_matrix = np.asmatrix(np.cov(data_matrix, rowvar=False))
+
+eigvals, eigvecs = np.linalg.eig(cov_matrix)
+eiglist = [(eigval, eigvec,) for eigval, eigvec in zip(eigvals, eigvecs)]
+eiglist.sort() # low to high
+eiglist.reverse() # high to low
+
+y = [val for val, _ in eiglist]
+pyplot.scatter([x for x in range(len(y))], y)
+pyplot.show()
+
 #
 # # print(len(eiglist))
 # # print([x[0] for x in eiglist])
