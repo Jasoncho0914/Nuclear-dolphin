40dd0e64b57d9a572e8f9465a4ae533cd8153763
Leo Tang <lt342@cornell.edu>
2017-11-03 00:08:53 -0400

    updated script

--- Scripts/script.py
+++ Scripts/script.py
@@ -4,6 +4,7 @@ import csv
 import time
 import sys
 import random
+import json
 from sklearn.cluster import KMeans
 from sklearn.cluster import DBSCAN
 from sklearn.cluster import AgglomerativeClustering
@@ -14,15 +15,15 @@ from sklearn.mixture import GaussianMixture
 from sklearn.manifold import SpectralEmbedding
 from sklearn.cluster import SpectralClustering
 from sklearn.metrics.pairwise import pairwise_kernels
+from sklearn.mixture import GaussianMixture
+from sklearn.cross_decomposition import CCA
+
 
 from Utility import assignClusters
 from Utility import createPred
 from Utility import writeCSV
 from Utility import initClusters
 from Utility import calculateCentriods
-from Utility import createLabeled
-from Utility import createTrain
-from Utility import runRandomForest
 from sklearn.semi_supervised import LabelSpreading
 from sklearn.semi_supervised import LabelPropagation
 from sklearn.ensemble import RandomForestClassifier
@@ -118,16 +119,16 @@ def createLabel(seed, data, graph):
     # writeCSV(embedding, 'embedding')
 
 
-    affinity = np.array(pd.read_csv("./Scripts/Results/affinity.csv", header=None))
-    embedding = np.array(pd.read_csv("./Scripts/Results/embedding.csv", header=None))
+    # affinity = np.array(pd.read_csv("./Scripts/Results/affinity.csv", header=None))
+    # embedding = np.array(pd.read_csv("./Scripts/Results/embedding.csv", header=None))
 
 
-    X = [x[:27] for x in embedding]
+    # X = [x[:35] for x in embedding]
     # X = data[:6000]
 
     #spectral clustering
-    clf = SpectralClustering(n_clusters=10, gamma=0.4)
-    labels = clf.fit_predict(X)
+    clf = SpectralClustering(n_clusters=10, affinity='precomputed')
+    labels = clf.fit_predict(affinity)
 
     #kmeans clustering
     # clusters = initClusters(seed, graph, 1)
@@ -136,6 +137,12 @@ def createLabel(seed, data, graph):
     # for i, val in enumerate(centriods):
     #     clusters[i] = [val]
     # centriods = calculateCentriods(data[:6000], clusters)
+
+    # X = _CCA(data[:6000], graph, 40)
+
+    # clf = GaussianMixture(n_components=10, init_params="random")
+    # clf.fit(X)
+    # labels = clf.predict(X)
 
     # kmeans = KMeans(n_clusters=10).fit(data[:6000])
     # labels = kmeans.labels_
@@ -147,44 +154,44 @@ def createLabel(seed, data, graph):
     assignClusters(seed, labels)
 
 
-    res = raw_input("Select number to assign: ").split(' ')
-    res2 = raw_input("Select digits to assign it to: ").split(' ')
-    if res[0] == "" or res2[0] == "":
-        print 'nothing'
-        return
+    # res = raw_input("Select number to assign: ").split(' ')
+    # res2 = raw_input("Select digits to assign it to: ").split(' ')
+    # if res[0] == "" or res2[0] == "":
+    #     print 'nothing'
+    #     return
 
-    assignment = {}
-    for i, val in enumerate(res):
-        assignment[int(val)] = int(res2[i])
-    print assignment 
+    # assignment = {}
+    # for i, val in enumerate(res):
+    #     assignment[int(val)] = int(res2[i])
+    # print assignment 
 
-    res = []
-    for i, label in enumerate(list(labels)):
-        if label in assignment:
-            res.append([i+1, assignment[label]])
-    print len(res)
+    # res = []
+    # for i, label in enumerate(list(labels)):
+    #     if label in assignment:
+    #         res.append([i+1, assignment[label]])
+    # print len(res)
 
-    name = ''
-    for num in res2:
-        name+=str(num)
-    writeCSV(res, 'TrainLabel_(' + name + ')')
+    # name = ''
+    # for num in res2:
+    #     name+=str(num)
+    # writeCSV(res, 'TrainLabel_(' + name + ')')
 
 
-def runRandomForest(train, test, ntree):
-    clf = RandomForestClassifier(ntree)
-    clf.fit(train[:, :-1], train[:, -1])
-    pred = clf.predict(test)
+# def runRandomForest(train, test, ntree):
+#     clf = RandomForestClassifier(ntree)
+#     clf.fit(train[:, :-1], train[:, -1])
+#     pred = clf.predict(test)
 
-    result = []
+#     result = []
 
-    d = {}
-    for i in range(6000, len(pred)):
-        c = d.setdefault(int(pred[i]), 0)
-        d[int(pred[i])] = c + 1
-        result.append([i+1, int(pred[i])])
+#     d = {}
+#     for i in range(6000, len(pred)):
+#         c = d.setdefault(int(pred[i]), 0)
+#         d[int(pred[i])] = c + 1
+#         result.append([i+1, int(pred[i])])
 
-    print d
-    return result
+#     print d
+#     return result
 
 
 def runLabelSpreading(data, assignment):
@@ -555,6 +562,16 @@ def combineLabels2(data, seed):
     writeCSV(res, 'assignment5')
 
 
+def _CCA(data, graph, n):
+    cca = CCA(n_components=n)
+    adjacencyMatrix = createAffinityMatrix(graph)
+    cca.fit(data, adjacencyMatrix)
+    X_c, Y_c = cca.transform(data, adjacencyMatrix)
+
+    writeCSV(X_c, 'CCA_X')
+    writeCSV(Y_c, 'CCA_Y')
+    # return X_c
+
 if __name__ == "__main__":
     path = sys.argv[1]
     output = sys.argv[2]
@@ -564,13 +581,13 @@ if __name__ == "__main__":
 
     print 'Creating assignment...'
     # data2 = np.array(pd.read_csv('./data/Extracted_Features.csv', header=None))
-    # start = time.time()
-    # createLabel(seed, data, graph)
+    start = time.time()
+    createLabel(seed, data, graph)
     # combineLabels2(data, seed)
-    assignment = np.array(pd.read_csv("./Scripts/Results/assignment5.csv", header=None))
-    # print time.time() - start 
+    # assignment = np.array(pd.read_csv("./Scripts/Results/assignment5.csv", header=None))
+    print time.time() - start 
     print 'Training...'
     # train = createTrain(data, assignment)
     # pred = runRandomForest(train, data, 50)
-    pred = runLabelSpreading(data, assignment)
-    writeCSV(pred, output, header=['Id', 'Label'])
+    # pred = runLabelSpreading(data, assignment)
+    # writeCSV(pred, output, header=['Id', 'Label'])
